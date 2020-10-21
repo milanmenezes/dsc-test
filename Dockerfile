45115c1f7f5554f284abc092b2de51b54b18d14b
milanmenezes <milanmenezes@gmail.com>
2020-10-21 12:35:54 +0530

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM tiangolo/uwsgi-nginx-flask
+COPY . /app
+RUN pip install -r requirements.txt
+ENV STATIC_URL /static
+ENV STATIC_PATH /app/static
---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,abort,jsonify,request,redirect,url_for
+
+from model import db,save_db
+
+app =Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("welcome.html",
+                           cards=db)
+
+@app.route("/card/<int:index>")
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html", card=card,index=index,max_index=len(db)-1)
+    except IndexError:
+        abort(404)
+
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+@app.route("/api/card/<int:index>")
+def api_card_details(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
+
+@app.route('/add_card/', methods=["GET" , "POST"])
+def add_card():
+    if request.method == "POST":
+        card = {"question" : request.form['question'],
+              "answer" : request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:
+        return render_template("create_card.html")
+
+if __name__ == '__main__':
+   app.run()
+
+
+
+
